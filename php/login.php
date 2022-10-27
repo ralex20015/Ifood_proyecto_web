31a2313aee7e8e6b75eeb6d8ab7f4e6e351bcc42
Alex Robles <lozanoeduardo8777@gmail.com>
2022-10-27 00:42:58 -0500

    Add recipes on the products page

--- php/login.php
+++ php/login.php
@@ -4,14 +4,15 @@
     $pass = $_POST["pass"];
 
     $consulta = mysqli_query($conn, "SELECT * FROM users where correo = '$email' && pass = '$pass'");
-
-    if ($rows = mysqli_fetch_array($consulta)) {
+    $rows = mysqli_fetch_array($consulta);
+    if ($rows) {
         session_start();
 
-        $_SESSION['nombre'] = $nombre;
+        $_SESSION['email'] = $email;
         header("location: ../pages/products.html");
     }else{
         header("location: ../pages/login.html");
+        echo"Error de autentificación";
     }
 
     mysqli_close($conn);
